9d169f054bb45819406aaf530c98475e3d7a057f
Lakshay Bhardwaj <lakshay@parth.ind.in>
2019-10-18 12:24:52 +0530

    changes done to fix Bug:Fixtures filtration is wrong in series wise tab for a particular category CE Android & iOS

--- ce-api-backend/series_retriever.php
+++ ce-api-backend/series_retriever.php
@@ -2,12 +2,19 @@
     header("Access-Control-Allow-Origin: *");
 	$memcache = new Memcached;
 	$s_id=$_REQUEST['id'];
+  $s_type=$_REQUEST['type'];
+
 	$seriesData = $memcache->get("series_".$s_id);
 	if($seriesData!=FALSE){
 		echo $seriesData;
 	}else{
 		require "conn.php";
-		$sql= "select * FROM um_matches WHERE series_name IN (SELECT series_name FROM um_series WHERE id = $s_id)";
+		//$sql= "select * FROM um_matches WHERE series_name IN (SELECT series_name FROM um_series WHERE id = $s_id)";
+    if($s_type!NULL){
+      $sql= "select * FROM um_matches WHERE series_name IN (SELECT series_name FROM um_series WHERE id = $s_id) and $s_type = 1";
+    }else{
+      $sql= "select * FROM um_matches WHERE series_name IN (SELECT series_name FROM um_series WHERE id = $s_id)";
+    }
 		$table = $conn->query($sql);
 		$arr= array();
 		while($value = $table->fetch_assoc()){
@@ -16,5 +23,5 @@
 		$json_value=json_encode($arr);
 		echo $json_value;
 		$memcache->set("series_".$s_id,$json_value,600);
-	}	
+	}
 ?>
